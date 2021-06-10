d5f4185d9c9c5814c223f0a9244bbb1edd1184db
NguyenTerry <ttn2@illinois.edu>
2021-06-09 21:03:15 -0500

    Add files via upload

---
+++ cs225/mp_mazes/dsets.cpp
@@ -0,0 +1,20 @@
+/* Your code here! */
+#include "dsets.h"
+
+void DisjointSets::addelements(int num) {
+
+}
+
+int DisjointSets::find(int elem) {
+    
+    return 0;
+}
+
+void DisjointSets::setunion(int a, int b) {
+
+}
+
+int DisjointSets::size(int elem) {
+
+    return 0;
+}
---
+++ cs225/mp_mazes/maze.cpp
@@ -0,0 +1,35 @@
+/* Your code here! */
+#include "maze.h"
+
+SquareMaze::SquareMaze() {
+
+}
+
+bool SquareMaze::canTravel(int x, int y, int dir) const {
+
+    return false;
+}
+
+PNG* SquareMaze::drawMaze() const {
+
+    return new PNG();
+}
+
+PNG* SquareMaze::drawMazeWithSolution() {
+
+    return new PNG();
+}
+
+void SquareMaze::makeMaze(int width, int height) {
+
+}
+
+void SquareMaze::setWall(int x, int y, int dir, bool exists) {
+
+}
+
+vector<int> SquareMaze::solveMaze() {
+
+    return vector<int>();
+}
+
